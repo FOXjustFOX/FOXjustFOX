bf903cb14f1a76f1bb01b7f570186cf79c80678c
FOXjustFOX <iglis.com20@gmail.com>
2025-03-21 19:38:40 +0100

    okey it works

--- my_cv/src/Home.tsx
+++ my_cv/src/Home.tsx
@@ -49,7 +49,7 @@ const Home: React.FC = () => {
                     }
                     transition={{ duration: 0.5, ease: "easeOut" }}>
                     <img
-                        src="assets/profile-photo.jpg"
+                        src="src/assets/profile-photo.jpg"
                         alt="Profile Photo"
                         className="profile-photo"
                     />
